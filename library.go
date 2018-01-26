e0c6cf3c38449c35a72317b7ec71768c342b4e9e
mohuishou <1@lailin.xyz>
2018-01-26 14:08:41 +0800

    【新增】当前借阅,历史借阅

--- library.go
+++ library.go
@@ -3,6 +3,11 @@ package library
 import (
 	"errors"
 	"net/url"
+	"reflect"
+	"strconv"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
 
 	"github.com/gocolly/colly"
 )
@@ -10,6 +15,19 @@ import (
 type Library struct {
 	URL string
 	c   *colly.Collector
+}
+
+// LoanBook 借阅的书籍
+type LoanBook struct {
+	Author      string
+	Title       string
+	PublishYear int     // 出版年
+	DueDate     string  // 到期日期
+	ReturnDate  string  // 归还日期(借阅历史)
+	ReturnTime  string  // 归还时间(借阅历史)
+	Arrearage   float64 // 欠费
+	Address     string  // 分馆
+	Number      string  // 索书号(当前借阅)
 }
 
 // NewLibrary 新建一个图书馆对象
@@ -37,14 +55,78 @@ func NewLibrary(studentID, password string) (*Library, error) {
 	return lib, nil
 }
 
-func (lib *Library) GetLoan() {
-	lib.c.OnHTML("body > center > center > table:nth-child(6) > tbody > tr", func (e *colly.HTMLElement)  {
-		
+// GetLoan 获取当前借阅
+func (lib *Library) GetLoan() []LoanBook {
+	books := make([]LoanBook, 0)
+	lib.c.OnHTML("body > center > center table", func(e *colly.HTMLElement) {
+		e.DOM.Find("tr").Each(func(i int, s *goquery.Selection) {
+			if s.Find("td:nth-child(6)").Text() == "" {
+				return
+			}
+			book := LoanBook{}
+			v := reflect.ValueOf(&book)
+			elem := v.Elem()
+			typeOfBook := elem.Type()
+			eq := 2
+			for k := 0; k < elem.NumField(); k++ {
+				val := strings.TrimSpace(s.Find("td").Eq(eq).Text())
+				switch typeOfBook.Field(k).Name {
+				case "ReturnDate", "ReturnTime":
+				case "PublishYear":
+					v, _ := strconv.Atoi(val)
+					elem.Field(k).SetInt(int64(v))
+					eq++
+				case "Arrearage":
+					v, _ := strconv.ParseFloat(val, 10)
+					elem.Field(k).SetFloat(v)
+					eq++
+				default:
+					elem.Field(k).SetString(val)
+					eq++
+				}
+			}
+			books = append(books, book)
+		})
 	})
+	lib.c.Visit(lib.URL + "?func=bor-loan&adm_library=SCU50")
+	return books
 }
 
-func (lib *Library) GetLoanAll() {
-
+// GetLoanAll 获取历史借阅
+func (lib *Library) GetLoanAll() []LoanBook {
+	books := make([]LoanBook, 0)
+	lib.c.OnHTML("body > center table", func(e *colly.HTMLElement) {
+		e.DOM.Find("tr").Each(func(i int, s *goquery.Selection) {
+			if s.Find("td:nth-child(6)").Text() == "" {
+				return
+			}
+			book := LoanBook{}
+			v := reflect.ValueOf(&book)
+			elem := v.Elem()
+			typeOfBook := elem.Type()
+			eq := 2
+			for k := 0; k < elem.NumField(); k++ {
+				val := strings.TrimSpace(s.Find("td").Eq(eq).Text())
+				switch typeOfBook.Field(k).Name {
+				case "Number":
+				case "PublishYear":
+					v, _ := strconv.Atoi(val)
+					elem.Field(k).SetInt(int64(v))
+					eq++
+				case "Arrearage":
+					v, _ := strconv.ParseFloat(val, 10)
+					elem.Field(k).SetFloat(v)
+					eq++
+				default:
+					elem.Field(k).SetString(val)
+					eq++
+				}
+			}
+			books = append(books, book)
+		})
+	})
+	lib.c.Visit(lib.URL + "?func=bor-history-loan&adm_library=SCU50")
+	return books
 }
 
 func (lib *Library) Loan() {
@@ -53,10 +135,6 @@ func (lib *Library) Loan() {
 
 func (lib *Library) LoanAll() {
 
-}
-
-func ()  {
-	
 }
 
 func getURL() string {
--- library_test.go
+++ library_test.go
@@ -7,3 +7,19 @@ import (
 func TestLibrary_getURL(t *testing.T) {
 	t.Log(getURL())
 }
+
+func TestLibrary_GetLoan(t *testing.T) {
+	lib, err := NewLibrary("xx", "xx")
+	if err != nil {
+		t.Fatal(err)
+	}
+	lib.GetLoan()
+}
+
+func TestLibrary_GetLoanAll(t *testing.T) {
+	lib, err := NewLibrary("xxx", "xxx")
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(lib.GetLoanAll())
+}
